112da77b930608b861d7cc6dbf45d22b50af7740
IgoRock <yellowcup.me@gmail.com>
2019-09-21 17:30:15 +0300

    + store | reducers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription, Observable, interval } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    selector    : 'app-root',
+    templateUrl : './app.component.html',
+    styleUrls   : ['./app.component.scss']
 })
 export class AppComponent {
     title = 'NgRxTest';
+
+    private intervalSubscription: Subscription;
+
+    interval$: Observable<any> = interval(1000);
+
+    start(): void {
+        this.intervalSubscription = this.interval$
+            .subscribe(() => console.log('pip'));
+    }
+
+    stop(): void {
+        this.intervalSubscription
+            .unsubscribe();
+    }
+
 }
